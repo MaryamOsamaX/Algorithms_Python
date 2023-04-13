166aea50c97fc7ce9f0295d090710f34fe4f81ce
Maryam Osama <45350444+MaryamOsamaX@users.noreply.github.com>
2023-04-13 14:25:56 +0200

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -1,6 +1,8 @@
 def gcd(x,y):
+    # Swap to the largest number for the division
     if x < y:
         x, y = y, x
+    # If Y = 0 then GCD(X,Y) = X
     if y == 0:
         return x
     return gcd(y, x%y)
